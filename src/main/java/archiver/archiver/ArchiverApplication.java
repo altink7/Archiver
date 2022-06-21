36d545bd277db1ab5bba595fb9fa845ec4832d76
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-06-21 16:24:13 +0200

    Initial commit

---
+++ src/main/java/archiver/archiver/ArchiverApplication.java
@@ -0,0 +1,13 @@
+package archiver.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/archiver/archiver/ArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package archiver.archiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
